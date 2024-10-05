e238b9dc3fb713a648e089678d52dd9908ce12ec
Valeria Pacheco <vpachecosanmartin@gmail.com>
2024-10-05 15:20:48 -0500

    Resolví todo el taller

--- src/com/inventario/model/ProductTableModel.java
+++ src/com/inventario/model/ProductTableModel.java
@@ -13,6 +13,40 @@ import java.util.ArrayList;
         }
 
         // Implementa los métodos requeridos (getRowCount, getColumnCount, getValueAt)
+
+        @Override
+        public int getRowCount() {
+            return productList.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            Product product = productList.get(rowIndex);
+            switch (columnIndex) {
+                case 0: return product.getId();
+                case 1: return product.getName();
+                case 2: return product.getPrice();
+                case 3: return product.getQuantity();
+                default: return null;
+            }
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            return columnNames[column];
+        }
+
         // Agrega el método addProduct
+
+        public void addProduct(Product product) {
+            productList.add(product);
+            fireTableRowsInserted(productList.size() - 1, productList.size() - 1);
+            // Notifica a la tabla que se ha insertado una nueva fila para que se actualice la vista
+        }
     }
 
